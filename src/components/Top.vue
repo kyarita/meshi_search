e0659914bc3695366645d6aa492b09afca7ea8bc
syathi <femto5401@gmail.com>
2018-12-06 01:18:47 +0900

    kyomu

---
+++ src/components/Top.vue
@@ -0,0 +1,85 @@
+<template>
+  <v-container>
+    <v-layout
+      text-xs-center
+      wrap
+    >
+      <v-flex mb-4>
+        <h1 class="display-2 font-weight-bold mb-3">
+          めしさーち 
+        </h1>
+      </v-flex>
+
+      <v-flex
+        mb-5
+        xs12
+      >
+        <h2 class="headline font-weight-bold mb-3">
+          Twitterで紹介されている美味そうなめしを検索できるよ！
+        </h2>
+      </v-flex>
+      <v-flex 
+        id="search"
+      >
+        <v-text-field
+          v-on:change="search"
+          label="search"
+          placeholder="ツイート内容で検索"
+          append-icon="search"
+        >
+        </v-text-field>
+      </v-flex>
+    </v-layout>
+    <v-layout
+     align-center 
+     justify-start
+    >
+      <div 
+        is="TweetCard" 
+        v-for="tweet in tweets" 
+        v-bind:key="tweet.id"
+        v-bind:picture="tweet.picture"
+        v-bind:text="tweet.text"
+      ></div>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+/* eslint-disable */
+  import TweetCard from './TweetCard'
+
+  export default {
+    name: 'Top',
+    components: {
+      TweetCard
+    },
+    props:[],
+    data() {
+      return {
+        tweets:[
+          {
+            id: 1,
+            picture: ["sample-img1.png"],
+            text: "SAMPLEsample1"
+          },
+          {
+            id: 2,
+            picture: ["sample-img2.png"],
+            text: "sampleSAMPLE1"
+          }
+        ]
+      };
+    },
+    methods: {
+      search: () => {
+        console.log(this);
+        this.tweets.push({id: 1, picture: "sample.png", text:"sample"});
+      }
+    }
+  } 
+</script>
+
+<style>
+
+</style>
---
+++ src/components/TweetCard.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-app class="card">
+    <v-container fluid grid-list-xs>
+      <v-layout>
+        <v-card>
+          <v-layout>
+            <v-flex xs12>
+              <v-layout>
+                <v-flex d-flex xl5 >
+                  <a href="https://www.yahoo.co.jp/">
+                    <v-img
+                      src="https://placehold.jp/150x150.png"
+                      max-height="150px"
+                      max-width="150px"
+                    ></v-img>
+                  </a>
+                </v-flex>
+              </v-layout>
+              <v-flex d-flex>
+                <v-flex v-for="n in 3" :key="n" d-flex>
+                  <a href="https://www.yahoo.co.jp/">
+                    <v-img
+                      src="https://placehold.jp/50x50.png"
+                      max-height="50px"
+                      max-width="50px"
+                    ></v-img>
+                  </a>
+                </v-flex>
+              </v-flex>
+            </v-flex>
+            <v-flex xs7>
+              <v-card-title class="text-content"> 
+                <a href="https://auctions.yahoo.co.jp/">
+                  <h4>飯処さんぷる</h4>
+                </a>
+                <a href="https://shopping.yahoo.co.jp/">
+                  <div>さんぷるサンプルsample見本見本</div>
+                </a>
+              </v-card-title>
+            </v-flex>
+          </v-layout>
+        </v-card>
+      </v-layout>
+    </v-container>
+  </v-app>  
+</template>
+
+<script>
+export default {
+  name: 'TweetCard',
+  data: () => ({})
+}
+</script>
+
+<style>
+  .card{
+    margin: 10px;
+  }
+  a{
+    color: black;
+    text-decoration: none;
+  }
+</style>
